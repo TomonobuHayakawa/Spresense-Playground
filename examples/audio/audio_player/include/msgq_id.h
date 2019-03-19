a44f2af8c2825f7491ce08ae9d85d4ef64a3c9b7
RyuutaSakane <38451491+RyuutaSakane@users.noreply.github.com>
2019-03-19 15:01:44 +0900

    Fit to latest SDK (#11)

--- examples/audio/audio_player/include/msgq_id.h
+++ examples/audio/audio_player/include/msgq_id.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * audio_player/include/msgq_id.h
+ * msgq_id.h
  *
  *   Copyright 2018 Sony Semiconductor Solutions Corporation
  *
