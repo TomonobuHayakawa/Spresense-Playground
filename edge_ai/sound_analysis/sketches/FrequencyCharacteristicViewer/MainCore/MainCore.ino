4ab3cbebdb7b5f31a1947dddc86a70bda8d805b9
TomonobuHayakawa <Tomonobu.Hayakawa@sony.com>
2023-01-22 02:07:58 +0900

    Add Frequency Characteristic Viewer.

---
+++ edge_ai/sound_analysis/sketches/FrequencyCharacteristicViewer/MainCore/MainCore.ino
@@ -0,0 +1,260 @@
+/*
+ *  Main.ino - Demo application .
+ *  Copyright 2023 T.Hayakawa
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <SDHCI.h>
+#include <SPI.h>
+
+#include <FrontEnd.h>
+#include <MemoryUtil.h>
+
+#include "CoreInterface.h"
+
+FrontEnd *theFrontEnd;
+SDClass theSD;
+
+// Audio parameters.
+static const int32_t channel_num  = AS_CHANNEL_MONO;
+static const int32_t bit_length   = AS_BITLENGTH_16;
+static const int32_t frame_sample = 1024;
+static const int32_t frame_size   = frame_sample * (bit_length / 8) * channel_num;
+static const int32_t mic_gain     = 160;
+
+static CMN_SimpleFifoHandle simple_fifo_handle;
+static const int32_t fifo_size  = frame_size * 20;
+static uint32_t fifo_buffer[fifo_size / sizeof(uint32_t)];
+
+static const int32_t proc_size  = frame_size;
+static uint8_t proc_buffer[proc_size];
+
+bool isEnd = false;
+bool ErrEnd = false;
+
+/* Multi-core parameters */
+const int proc_core = 1;
+const int view_core = 2;
+
+/**
+ * @brief Frontend attention callback
+ *
+ * When audio internal error occurc, this function will be called back.
+ */
+
+void frontend_attention_cb(const ErrorAttentionParam *param)
+{
+  puts("Attention!");
+
+  if (param->error_code >= AS_ATTENTION_CODE_WARNING) {
+    ErrEnd = true;
+  }
+}
+
+/**
+ * @brief Frontend done callback procedure
+ *
+ * @param [in] event        AsMicFrontendEvent type indicator
+ * @param [in] result       Result
+ * @param [in] sub_result   Sub result
+ *
+ * @return true on success, false otherwise
+ */
+
+static bool frontend_done_callback(AsMicFrontendEvent ev, uint32_t result, uint32_t sub_result)
+{
+  UNUSED(ev);
+  UNUSED(result);
+  UNUSED(sub_result);
+  return true;
+}
+
+/**
+ * @brief Frontend pcm capture callback procedure
+ *
+ * @param [in] pcm          PCM data structure
+ *
+ * @return void
+ */
+static void frontend_pcm_callback(AsPcmDataParam pcm)
+{
+  if (!pcm.is_valid) {
+    puts("Invalid data !");
+    return;
+  }
+
+  if (CMN_SimpleFifoGetVacantSize(&simple_fifo_handle) < pcm.size) {
+    puts("Simple FIFO is full !");
+    return;
+  }
+
+  if (CMN_SimpleFifoOffer(&simple_fifo_handle, (const void*)(pcm.mh.getPa()), pcm.size) == 0) {
+    puts("Simple FIFO is full !");
+    return;
+  }
+
+  if (pcm.is_end) {
+    isEnd = true;
+  }
+
+  return;
+}
+
+/**
+ *  @brief Setup audio device to capture PCM stream
+ *
+ *  Select input device as microphone <br>
+ *  Set PCM capture sapling rate parameters to 48 kb/s <br>
+ *  Set channel number 4 to capture audio from 4 microphones simultaneously <br>
+ */
+void setup()
+{
+  Serial.begin(115200);
+
+  /* Initialize memory pools and message libs */
+  initMemoryPools();
+  createStaticPools(MEM_LAYOUT_RECORDER);
+
+  if (CMN_SimpleFifoInitialize(&simple_fifo_handle, fifo_buffer, fifo_size, NULL) != 0) {
+    print_err("Fail to initialize simple FIFO.\n");
+    exit(1);
+  }
+  
+  /* Launch SubCore */
+  int ret = MP.begin(proc_core);
+  if (ret < 0) {
+    printf("MP.begin error = %d\n", ret);
+  }
+  
+  ret = MP.begin(view_core);
+  if (ret < 0) {
+    printf("MP.begin error = %d\n", ret);
+  }
+  
+  /* receive with non-blocking */
+  MP.RecvTimeout(1);
+
+  /* Use SD card */
+  theSD.begin();
+
+  /* start audio system */
+  theFrontEnd = FrontEnd::getInstance();
+  theFrontEnd->begin(frontend_attention_cb);
+
+  puts("initialization FrontEnd");
+
+  /* Set capture clock */
+  theFrontEnd->setCapturingClkMode(FRONTEND_CAPCLK_NORMAL);
+
+  /* Activate Objects. Set output device to Microphone */
+  theFrontEnd->activate(frontend_done_callback);
+
+  usleep(100 * 1000); /* waiting for Mic startup */
+
+  /* Initialize of capture */
+  AsDataDest dst;
+  dst.cb = frontend_pcm_callback;
+
+  theFrontEnd->init(channel_num,
+                    bit_length,
+                    frame_sample,
+                    AsDataPathCallback,
+                    dst);
+
+  theFrontEnd->setMicGain(mic_gain);
+
+  theFrontEnd->start();
+  puts("Capturing Start!");
+
+}
+
+/**
+ * @brief Capture a frame of PCM data into buffer for signal processing
+ */
+bool execute_aframe()
+{
+  int8_t sndid = 100; /* user-defined msgid */
+  static Request request;
+
+  size_t size = CMN_SimpleFifoGetOccupiedSize(&simple_fifo_handle);
+
+  if (size > 0) {
+    if (size > proc_size) {
+      size = (size_t)proc_size;
+    }
+
+    if (CMN_SimpleFifoPoll(&simple_fifo_handle, (void*)proc_buffer, size) == 0) {
+      printf("ERROR: Fail to get data from simple FIFO.\n");
+      return false;
+    }
+
+    request.buffer  = proc_buffer;
+    request.sample  = size / 2;
+
+    MP.Send(sndid, &request, proc_core);
+
+  }
+
+  return true;
+}
+
+
+/**
+ * @brief Main loop
+ */
+
+void loop() {
+
+  static Request request;
+  
+  /* Execute audio data */
+  if (!execute_aframe()) {
+    puts("Capturing Error!");
+    ErrEnd = true;
+  }
+
+  int8_t   rcvid;
+  Result*  result;
+  int8_t   sndid = NOMAL_REQUEST_ID;
+
+  int ret = MP.Recv(&rcvid, &result, proc_core);
+  if(ret >= 0){
+    request.buffer  = result->buffer;
+    request.sample  = result->sample;
+    MP.Send(sndid, &request, view_core);
+  }
+
+  if (isEnd) {
+    theFrontEnd->stop();
+    goto exitCapturing;
+  }
+
+  if (ErrEnd) {
+    puts("Error End");
+    theFrontEnd->stop();
+    goto exitCapturing;
+  }
+
+  return;
+
+exitCapturing:
+  theFrontEnd->deactivate();
+  theFrontEnd->end();
+
+  puts("End Capturing");
+  exit(1);
+
+}
---
+++ edge_ai/sound_analysis/sketches/FrequencyCharacteristicViewer/ViewerCore/ViewerCore.ino
@@ -0,0 +1,171 @@
+/*
+ *  ViewCore.ino - Demo application.
+ *  Copyright 2023 T.Hayakawa
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <string.h>
+
+#include <SPI.h>
+
+#include "CoreInterface.h"
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+// Graphics parameters.
+// For the Adafruit shield, these are the default.
+#define TFT_DC 9
+#define TFT_CS 10
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+/* Multi-core parameters */
+const int subcore = 2;
+
+/*int screen_width;
+int screen_height; */
+
+#define SCREEN_WIDTH  320
+#define SCREEN_HEIGHT 240
+
+#define LEFT_SPACE   30
+#define RIGHT_SPACE  10
+#define TOP_SPACE    40
+#define BOTTOM_SPACE 10
+
+#define GRAPH_WIDTH  (SCREEN_WIDTH - LEFT_SPACE - RIGHT_SPACE)
+#define GRAPH_HEIGHT (SCREEN_HEIGHT - TOP_SPACE - BOTTOM_SPACE)
+
+#define FRAME_NUMBER 2
+
+
+struct FrameInfo{
+  float buffer[GRAPH_WIDTH];
+  bool renable;
+  bool wenable;
+  FrameInfo():renable(false),wenable(true){}
+};
+
+FrameInfo frame_buffer[FRAME_NUMBER];
+
+
+
+/**
+ *  @brief Setup audio device to capture PCM stream
+ *
+ *  Select input device as microphone <br>
+ *  Set PCM capture sapling rate parameters to 48 kb/s <br>
+ *  Set channel number 4 to capture audio from 4 microphones simultaneously <br>
+ */
+void setup()
+{
+  Serial.begin(115200);
+
+  /* Initialize MP library */
+  int ret = MP.begin();
+  if (ret < 0) {
+    errorLoop(2);
+  }
+  
+  /* receive with non-blocking */
+  MP.RecvTimeout(1);
+
+  puts("Graph Start");
+  tft.begin(40000000);
+  
+/*  screen_width = tft.width();
+  screen_height = tft.height(); */
+
+  tft.fillScreen(ILI9341_BLACK);
+
+  draw_frame(ILI9341_WHITE);
+  Serial.println("Done!");
+
+  int proter = task_create("Proter", 70, 0x400, proter_task, (FAR char* const*) 0);
+
+}
+
+int proter_task(int argc, FAR char *argv[])
+{
+  while(1){
+    for(int i=0;i<FRAME_NUMBER;i++){
+      if(frame_buffer[i].renable){
+        frame_buffer[i].renable = false;
+        prot_data(frame_buffer[i].buffer,ILI9341_RED);
+        frame_buffer[i].wenable = true;
+      }
+    }
+  }
+}
+
+void loop()
+{
+  int      ret;
+  int8_t   rcvid;
+  Request  *request;
+  
+  /* Receive PCM captured buffer from MainCore */
+  ret = MP.Recv(&rcvid, &request);
+  if (ret >= 0) {
+     write_buffer(request->buffer);
+  }
+
+}
+
+int write_buffer(float* data)
+{
+  for(int i=0;i<FRAME_NUMBER;i++){
+    if(frame_buffer[i].wenable){
+      frame_buffer[i].wenable = false;
+      memcpy(frame_buffer[i].buffer,data,GRAPH_WIDTH*sizeof(float)/2);
+      frame_buffer[i].renable =true;
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void draw_frame(uint16_t color)
+{
+  tft.drawRect(BOTTOM_SPACE, LEFT_SPACE, GRAPH_HEIGHT, GRAPH_WIDTH, color);
+}
+
+inline float f_max(float x, float y) { return((x>y)?x:y); }
+inline float f_min(float x, float y) { return((x<y)?x:y); }
+
+void prot_data(float* data, uint16_t color)
+{
+  tft.fillRect(BOTTOM_SPACE+1, LEFT_SPACE+1, GRAPH_HEIGHT-2, GRAPH_WIDTH-2, ILI9341_BLACK);
+  for(int i=1;i<GRAPH_WIDTH/2-2;i++){
+    tft.drawLine(BOTTOM_SPACE+f_min((*data),(GRAPH_HEIGHT-2)),LEFT_SPACE+i*2,BOTTOM_SPACE+f_min(*(data+1),GRAPH_HEIGHT-2),LEFT_SPACE+(i+1)*2, color);
+    data++;
+  }
+}
+
+void errorLoop(int num)
+{
+  int i;
+
+  while (1) {
+    for (i = 0; i < num; i++) {
+      ledOn(LED0);
+      delay(300);
+      ledOff(LED0);
+      delay(300);
+    }
+    delay(1000);
+  }
+}
