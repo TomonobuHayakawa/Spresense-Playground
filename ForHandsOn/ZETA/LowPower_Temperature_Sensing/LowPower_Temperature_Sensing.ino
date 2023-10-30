dc3c29324d68f2b92e629c15eac0ba11b4a178e0
TomonobuHayakawa <Tomonobu.Hayakawa@sony.com>
2023-10-31 03:06:05 +0900

    Add samples for ZETA handson.

---
+++ ForHandsOn/ZETA/LowPower_Temperature_Sensing/LowPower_Temperature_Sensing.ino
@@ -0,0 +1,174 @@
+/******************************************************************************
+Example2_One-Shot_Temperature_Reading.ino
+Example for the TMP102 I2C Temperature Sensor
+Alex Wende @ SparkFun Electronics
+April 29th 2016
+~
+
+This sketch connects to the TMP102 temperature sensor and enables the
+one-shot temperature measurement mode using the oneShot() function.
+The function returns 0 until the temperature measurement is ready to
+read (takes around 25ms). After the measurment is read, the TMP102 is
+placed back into sleep mode before the loop is repeated. This can be 
+useful to save power or increase the continuous conversion rate from
+8Hz up to a maximum of 40Hz.
+
+Resources:
+Wire.h (included with Arduino IDE)
+SparkFunTMP102.h
+
+Development environment specifics:
+Arduino 1.0+
+
+This code is beerware; if you see me (or any other SparkFun employee) at
+the local, and you've found our code helpful, please buy us a round!
+
+Distributed as-is; no warranty is given.   
+******************************************************************************/
+
+// Include the SparkFun TMP102 library.
+// Click here to get the library: http://librarymanager/All#SparkFun_TMP102
+
+#include <Wire.h> // Used to establied serial communication on the I2C bus
+#include <SparkFunTMP102.h> // Used to send and recieve specific information from our sensor
+#include <LowPower.h>
+#include "TZM902DP.h"
+
+TZM902DP Zeta;
+
+// Connections
+// VCC = 3.3V
+// GND = GND
+// SDA = A4
+// SCL = A5
+
+TMP102 sensor0;
+// Sensor address can be changed with an external jumper to:
+// ADD0 - Address
+//  VCC - 0x49
+//  SDA - 0x4A
+//  SCL - 0x4B
+
+const char* boot_cause_strings[] = {
+  "Power On Reset with Power Supplied",
+  "System WDT expired or Self Reboot",
+  "Chip WDT expired",
+  "WKUPL signal detected in deep sleep",
+  "WKUPS signal detected in deep sleep",
+  "RTC Alarm expired in deep sleep",
+  "USB Connected in deep sleep",
+  "Others in deep sleep",
+  "SCU Interrupt detected in cold sleep",
+  "RTC Alarm0 expired in cold sleep",
+  "RTC Alarm1 expired in cold sleep",
+  "RTC Alarm2 expired in cold sleep",
+  "RTC Alarm Error occurred in cold sleep",
+  "Unknown(13)",
+  "Unknown(14)",
+  "Unknown(15)",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "GPIO detected in cold sleep",
+  "SEN_INT signal detected in cold sleep",
+  "PMIC signal detected in cold sleep",
+  "USB Disconnected in cold sleep",
+  "USB Connected in cold sleep",
+  "Power On Reset",
+};
+
+void printBootCause(bootcause_e bc)
+{
+  Serial.println("--------------------------------------------------");
+  Serial.print("Boot Cause: ");
+  Serial.print(boot_cause_strings[bc]);
+  if ((COLD_GPIO_IRQ36 <= bc) && (bc <= COLD_GPIO_IRQ47)) {
+    // Wakeup by GPIO
+    int pin = LowPower.getWakeupPin(bc);
+    Serial.print(" <- pin ");
+    Serial.print(pin);
+  }
+  Serial.println();
+  Serial.println("--------------------------------------------------");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(); //Join I2C Bus
+
+  // Initialize LowPower library
+  LowPower.begin();
+
+  // Initialize ZETA library
+  Zeta.begin();
+
+  // Get the boot cause
+  bootcause_e bc = LowPower.bootCause();
+
+  // Print the boot cause
+  printBootCause(bc);
+
+  uint32_t wait_time = Zeta.connect();
+  sleep(wait_time);
+
+  sleep(10);
+
+  if ((bc == POR_SUPPLY) || (bc == POR_NORMAL)) {
+    Zeta.activate();
+  }
+
+  /* The TMP102 uses the default settings with the address 0x48 using Wire.
+  
+     Optionally, if the address jumpers are modified, or using a different I2C bus,
+   these parameters can be changed here. E.g. sensor0.begin(0x49,Wire1)
+
+   It will return true on success or false on failure to communicate. */
+  if(!sensor0.begin())
+  {
+    Serial.println("Cannot connect to TMP102.");
+    Serial.println("Is the board connected? Is the device ID correct?");
+    while(1);
+  }
+  
+  Serial.println("Connected to TMP102!");  
+  delay(100);
+  sensor0.sleep(); // Put sensor to sleep
+}
+ 
+void loop()
+{
+  char packet[4];
+
+  sensor0.oneShot(1); // Set One-Shot bit
+
+  while(sensor0.oneShot() == 0); // Wait for conversion to be ready
+  float temp = sensor0.readTempC();
+  Serial.println(temp);
+  sensor0.sleep(); // Return sensor to sleep
+
+  uint32_t palam = temp * 10;
+  
+  //温度データをpacketへの格納　JAZE温湿度センサJZTH91A1に準拠
+  packet[0] = 01;//ヘッダ
+  packet[1] = (palam & 0x0000FF00) >> 8;//温度
+  packet[2] = (palam & 0x000000FF);//温度
+  packet[3] = 00;//（湿度実装なし）
+
+  Zeta.send(packet, sizeof(packet));
+  sleep(20);
+
+  Serial.println("Go to deep sleep...");
+  Zeta.downlink_check();
+  Zeta.end();
+
+  LowPower.deepSleep(30);
+
+}
