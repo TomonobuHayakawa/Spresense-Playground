7c9f5d73134ce7408ae035bc44108ea557c6545b
TomonobuHayakawa <Tomonobu.Hayakawa@sony.com>
2025-09-10 22:13:41 +0900

    Update script for generating training dataset.

--- IntegrationWithProcessing/AiCamera/NumberRecognition/data_set/index_writer.py
+++ IntegrationWithProcessing/AiCamera/NumberRecognition/data_set/index_writer.py
@@ -1,49 +1,42 @@
 from PIL import Image
 import random
 import os
-
+import sys
 from pathlib import Path
 
-types_list = {0,1,2,3};
-image_number = 100;
+directory_name = sys.argv[1] if len(sys.argv) > 1 else "."
 
-types_number = len(types_list);
-
-filenpath = [];
-data_path = "./data"
+types_list = {0,1,2,3,4,5,6,7,8,9,10}
+types_number = len(types_list)
+data_path = "data"
 
 def make_data_dir(types):
-    if not os.path.isdir(data_path):
-      os.mkdir("data")
+    os.makedirs(os.path.join(directory_name, data_path), exist_ok=True)
+    for i in range(types):
+        os.makedirs(os.path.join(directory_name, data_path, str(i)), exist_ok=True)
 
-    for i in range(0,types):
-      type_path = data_path + "/" + str(i)
-      if not os.path.isdir(type_path):
-        os.mkdir(type_path)
-
-def write_to_file(types, count,filename):
+def write_to_file(types, filename):
+    filenpath = []
     with open(filename, 'w') as file:
         file.write("x:image,y:label\n")
-        for i in range(0,types):
-            type_path = Path(data_path + "/" + str(i))
-
-            for img in type_path.iterdir():
-              if img.is_file() and img.suffix.lower() in ".bmp":
-                image = Image.open(img)
-                pngname = str(img)[:-4] + ".png"
-                image.save(pngname)
-                filenpath.append(f"{pngname},{i}\n")
-              elif img.is_file() and img.suffix.lower() in ".png":
-                filenpath.append(f"{type_path}\\{img.name},{i}\n")
-
+        for i in range(types):
+            type_path = Path(os.path.join(directory_name, data_path, str(i)))
+            for img_path in type_path.iterdir():
+                if img_path.is_file() and img_path.suffix.lower() == ".bmp":
+                    png_path = img_path.with_suffix(".png")
+                    image = Image.open(img_path)
+                    image.save(png_path)
+                    image.close()
+                    os.remove(img_path)
+                    rel_path = os.path.relpath(png_path, directory_name)
+                    filenpath.append(f"{rel_path},{i}\n")
+                elif img_path.is_file() and img_path.suffix.lower() == ".png":
+                    rel_path = os.path.relpath(img_path, directory_name)
+                    filenpath.append(f"{rel_path},{i}\n")
         random.shuffle(filenpath)
+        file.writelines(filenpath)
 
-        for path in filenpath:
-            file.write(path)
-
-output_filename = "index.csv"
+output_filename = os.path.join(directory_name, "index.csv")
 make_data_dir(types_number)
-write_to_file(types_number, image_number, output_filename)
-
+write_to_file(types_number, output_filename)
 print(f"{output_filename} done.")
-
